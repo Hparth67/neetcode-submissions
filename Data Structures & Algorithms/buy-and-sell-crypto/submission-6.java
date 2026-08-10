e5e307a4ac10eda21dc12a140658a0cc53cf1171
Hparth67 <152985137+Hparth67@users.noreply.github.com>
2026-08-10 18:29:32 +0530

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int i = 0;
+        int j = i + 1;
+        int max = 0;
+        while(j < prices.length) {
+            if(prices[i] >= prices[j]) {
+                i = j;
+                j++;
+            }
+            else {
+                max = Math.max(max, prices[j] - prices[i]);
+                j++;
+            }
+        }
+        return max;
+    }
+}
